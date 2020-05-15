62c4a05aa3864d43fa97441b4cb3050280b4f3e3
Yorxika <774123033@qq.com>
2020-05-16 01:54:03 +0800

    finish lab 1

---
+++ lab1/test.c
@@ -0,0 +1,38 @@
+char c1[2][3][4];
+int main(){
+    int i,j,k;
+    float x,y;
+    i+=-i*++j;
+    c[1][2]=10.12;
+    for(int i = 0;i < 10; i++)
+    {
+    	if(i != j) break;
+    	else continue;
+    };
+    return 'a';
+}
+
+
+int i,j;
+float m,n;
+float fun(int x,char y)
+{
+    int a[10],b[10][20];
+    i+j=b[3,5];
+    j=0789;
+    m=0xAB;
+    return 1;
+
+}
+
+float test1(int x,char y)
+{   int a,b;  //注释1
+ /*  注释2
+     a=0；//
+*/  a='a';
+      b='\n';
+    a*/*b+x*/b;
+	char a;
+    return 1;
+}
+
